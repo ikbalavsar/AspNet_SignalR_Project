ac306fca1fe83504987a5053e3a79337f787f837
Gülsüm İkbal Avşar <gulsumikbalavsar@gmail.com>
2023-11-25 15:54:23 +0300

    Initial Commit

---
+++ AspNet_SignalR_Project/SignalR_Api/DAL/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR_Api.DAL.Entities
+{
+    public class Category
+    {
+        public int CategoryID{ get; set; }
+        public string Name{ get; set; }
+        public bool Status{ get; set; }
+    }
+}
---
+++ AspNet_SignalR_Project/SignalR_Web_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalR_Web_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
