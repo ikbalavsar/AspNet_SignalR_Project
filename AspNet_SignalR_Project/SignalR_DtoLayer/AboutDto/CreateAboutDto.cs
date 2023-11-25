5f17a396bd9f192e396caf4dae4cf6115b22990e
Gülsüm İkbal Avşar <gulsumikbalavsar@gmail.com>
2023-11-25 22:27:24 +0300

    Dto's are created.

---
+++ AspNet_SignalR_Project/SignalR_DtoLayer/AboutDto/CreateAboutDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR_DtoLayer.AboutDto
+{
+    public class CreateAboutDto
+    {
+        public string ImageURl { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
