38600b66f9d20de9fc1ea0989f257645092b6de9
Gülsüm İkbal Avşar <gulsumikbalavsar@gmail.com>
2023-12-15 21:18:23 +0300

    Admin Panel Feature / Listing - Create - Update

--- AspNet_SignalR_Project/SignalR_Web_UI/Dtos/FeatureDtos/GetFeatureDto.cs
+++ AspNet_SignalR_Project/SignalR_Web_UI/Dtos/FeatureDtos/GetFeatureDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SignalR_DtoLayer.FeatureDto
+namespace SignalR_Web_UI.Dtos.FeatureDtos
 {
     public class GetFeatureDto
     {
