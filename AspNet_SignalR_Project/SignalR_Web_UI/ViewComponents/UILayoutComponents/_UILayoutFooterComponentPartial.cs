0a7fac5baa33044ee2b4a79ae09c823f97a99099
Gülsüm İkbal Avşar <98840546+ikbalavsar@users.noreply.github.com>
2024-01-20 18:16:28 +0300

    Home Page Slider Partial View

---
+++ AspNet_SignalR_Project/SignalR_Web_UI/ViewComponents/UILayoutComponents/_UILayoutFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_Web_UI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutFooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
