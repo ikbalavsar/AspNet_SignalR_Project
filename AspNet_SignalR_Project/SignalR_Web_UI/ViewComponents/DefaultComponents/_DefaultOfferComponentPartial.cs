87f7f549226e639f4e0e88f2725ebc7b23cc309b
Gülsüm İkbal Avşar <98840546+ikbalavsar@users.noreply.github.com>
2024-01-23 21:01:18 +0300

    Home Page Discount part is completed.

--- AspNet_SignalR_Project/SignalR_Web_UI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
+++ AspNet_SignalR_Project/SignalR_Web_UI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR_Web_UI.Dtos.DiscountDtos;
+using SignalR_Web_UI.Dtos.SliderDtos;
 
 namespace SignalR_Web_UI.ViewComponents.DefaultComponents
 {
     public class _DefaultOfferComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultOfferComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/Discount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+                return View(values);
+            }
             return View();
+
         }
     }
 }
--- AspNet_SignalR_Project/SignalR_Web_UI/obj/Debug/net8.0/SignalR_Web_UI.AssemblyInfo.cs
+++ AspNet_SignalR_Project/SignalR_Web_UI/obj/Debug/net8.0/SignalR_Web_UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR_Web_UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab6c88fed70b25a936551c12a2e91f31c222f2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b005ce639135723dad520ae24d6bf5a64b05ae17")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR_Web_UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalR_Web_UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
